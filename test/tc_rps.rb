757bac734db59945c68964424c74c477b18fb929
Carlos Glez <carlosgc1989@gmail.com>
2012-10-22 16:01:33 +0100

    test 1

--- test/tc_rps.rb
+++ test/tc_rps.rb
@@ -1,42 +1,42 @@
 require 'rps'
-require "test/unit"
+require 'test/unit'
 
 class TestRockPaperScissors < Test::Unit::TestCase
+def setup
+#def test variable
+end
 
-  def setup
-		#def test variable
-	end
+def teardown
+#nothing to do
+end
 
-	def teardown
-		#nothing to do
-	end
-#***********************************test de juego**************************
-	def test_play_1
-		30.times do
-			assert correct_answer(RockPaperScissors.play('scissors'))
-		end	
-	end #acaba test_play
-  
-	def test_play_2
-		30.times do
-			assert correct_answer(RockPaperScissors.play('paper'))
-  		end #acaba test_play
-  end
+def test_wrong_play
+assert_raise (SyntaxError){RockPaperScissors.play('Fuego')}
+end
 
-  def test_play_3
-    30.times do
-  		assert correct_answer(RockPaperScissors.play('rock'))
-  	end  	
-	end #acaba test_play
+def correct_answer(x)
+return true if x =~ /Empate/
+return true if x =~ /Gana maquina: (\w+) defeats (\w+)/ and RockPaperScissors.defeat[$1.to_sym] == $2.to_sym
+return true if x =~ /Gana jugador: (\w+) beats (\w+)/ and RockPaperScissors.defeat[$1.to_sym] == $2.to_sym
 
-#******************** Wrong play ****************************************
- 	def test_wrong_play
-  	assert_raises(SyntaxError){RockPaperScissors.play('huracan')}
- 	end
+end
 
-#********** Ver si las respuestas no se repiten *************************
- 	def correct_answer(x)
- 		return true if x =- /Empate: /
-		#   return true if x =-  /Gana maquina. Maquina #{computer_throw} versus Jugador #{player_throw} /    
- 	end
+def test_play_1
+30.times do
+assert correct_answer(RockPaperScissors.play('tijeras')) 
+end
+end
+
+def test_play_2
+30.times do
+assert correct_answer(RockPaperScissors.play('papel'))
+end
+end
+
+def test_play_3
+30.times do
+assert correct_answer(RockPaperScissors.play('piedra'))
+end
+end
+
 end
